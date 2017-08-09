4550866ceee50cc79d93e072775dda60f5dc5305
bjaminwiz@gmail.com <benwisinski@Bens-MacBook-Air.local>
2017-08-08 22:31:46 -0600

    resturctured to be actual test

--- spec/subscription_spec.rb
+++ spec/subscription_spec.rb
@@ -1,10 +1,13 @@
 require "../recurly/create_subscription"
 
 RSpec.describe Subscription_Test do
-  describe "#score" do
-    it "returns 0 for an all gutter game" do
+  describe "#sub_count" do
+    it "returns count of subscriptions found against a fresh account" do
       sub_test = Subscription_Test.new
-      expect(sub_test.sub_count).to eq(0)
+      sub_test.create_account("1235")
+      expect(sub_test.sub_count("1235")).to eq(0)
+      sub_test.create_sub("1235")
+      expect(sub_test.sub_count("1235")).to eq(1)
     end
   end
 end
